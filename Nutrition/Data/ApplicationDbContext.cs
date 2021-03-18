ebe8a4cd1b53c1676ccbb495ff1bb1cfdabc3fc2
Искра Тенева <iskra.teneva3@gmail.com>
2021-03-18 21:27:52 +0200

    18.03-User UI

--- Nutrition/Data/ApplicationDbContext.cs
+++ Nutrition/Data/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Nutrition.Models;
+
 
 namespace Nutrition.Data
 {
@@ -12,7 +9,9 @@ namespace Nutrition.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            Database.EnsureCreated();
         }
+
         public DbSet<Nutrition.Models.Meal> Meal { get; set; }
         public DbSet<Nutrition.Models.Diary> Diary { get; set; }
         public DbSet<Nutrition.Models.User> User { get; set; }
